fc2e5a28bde30e89389c50462b458342c5c84f9e
Peter Emad <peteremads1406@gmail.com>
2026-07-08 16:11:57 +0300

    Assembled the Sideband Packet Header according to the type'

--- Logical_PHY/lphy_sb_pkt_enc.sv
+++ Logical_PHY/lphy_sb_pkt_enc.sv
@@ -35,21 +35,21 @@ module lphy_sb_pkt_enc(
   output logic o_lphy_sb_pkt_enc_pkt_has_data
 );
 
-  logic is_reg_req;
-  logic is_reg_cpl;
-  logic is_msg;
-  logic has_data;
-  logic [31:0] phase0_reg;
-  logic [31:0] phase1_reg;
-  logic [63:0] raw_header;
-  logic [63:0] calc_header;
+  logic internal_is_reg_req;
+  logic internal_is_reg_cpl;
+  logic internal_is_msg;
+  logic internal_has_data;
+  logic [31:0] internal_phase0_reg;
+  logic [31:0] internal_phase1_reg;
+  logic [63:0] internal_raw_header;
+  logic [63:0] internal_calc_header;
 
   // Decode Opcode to determine packet type and data presense (Table 47)
   always_comb begin
-    is_msg = (i_lphy_sb_pkt_enc_opcode == 5'b10010) || 
+    internal_is_msg = (i_lphy_sb_pkt_enc_opcode == 5'b10010) || 
              (i_lphy_sb_pkt_enc_opcode == 5'b11011);
 
-    has_data = (i_lphy_sb_pkt_enc_opcode == 5'b00001) ||   // 32b Mem Write
+    internal_has_data = (i_lphy_sb_pkt_enc_opcode == 5'b00001) ||   // 32b Mem Write
                (i_lphy_sb_pkt_enc_opcode == 5'b00101) ||   // 32b Cfg Write
                (i_lphy_sb_pkt_enc_opcode == 5'b01001) ||   // 64b Mem Write
                (i_lphy_sb_pkt_enc_opcode == 5'b01101) ||   // 64b Cfg Write
@@ -57,7 +57,7 @@ module lphy_sb_pkt_enc(
                (i_lphy_sb_pkt_enc_opcode == 5'b11001) ||   // Cpl with 64b Data
                (i_lphy_sb_pkt_enc_opcode == 5'b11011) ||   // Message with 64b Data
 
-    is_reg_req = (i_lphy_sb_pkt_enc_opcode == 5'b00000) ||
+    internal_is_reg_req = (i_lphy_sb_pkt_enc_opcode == 5'b00000) ||
                  (i_lphy_sb_pkt_enc_opcode == 5'b00001) ||
                  (i_lphy_sb_pkt_enc_opcode == 5'b00100) ||
                  (i_lphy_sb_pkt_enc_opcode == 5'b00101) ||
@@ -66,9 +66,42 @@ module lphy_sb_pkt_enc(
                  (i_lphy_sb_pkt_enc_opcode == 5'b01100) ||
                  (i_lphy_sb_pkt_enc_opcode == 5'b01101);
     
-    is_reg_cpl = (i_lphy_sb_pkt_enc_opcode == 5'b10000);
+    internal_is_reg_cpl = (i_lphy_sb_pkt_enc_opcode == 5'b10000);
                  (i_lphy_sb_pkt_enc_opcode == 5'b10001);
                  (i_lphy_sb_pkt_enc_opcode == 5'b11001);
   end
+
+  // Assemble the 64-bit Header (Phase 0 and Phase 1)
+  always_comb begin
+    if (internal_is_msg) begin
+      // Message Format 
+      // Phase 0: srcid [31:29], rsvd[28:22], msgcode[21:14], rsvd[13:5], opcode[4:0]
+      internal_phase0_reg = {i_lphy_sb_pkt_enc_srcid, 7'h00, i_lphy_sb_pkt_enc_msgcode, 
+                             9'h000, i_lphy_sb_pkt_enc_opcode};
+      // Phase 1: dp[31], cp[30], rsvd[29:27], dstid[26:24], msginfo[23:8], msgsubcode[7:0]
+      // Note: dp and cp are left 0 here; the will be calculated by the CRC block.
+      internal_phase1_reg = {2'b00, 3'b000, i_lphy_sb_pkt_enc_dstid, i_lphy_sb_pkt_enc_msginfo,
+                             i_lphy_sb_pkt_enc_msgsubcode};
+    end else if (internal_is_reg_cpl) begin
+      // Register Access Completions
+      // Phase 0: srcid[31:28], rsvd[28:27], tag[26:22], be[21:14], rsvd[13:6], ep[5], opcode[4:0]
+      internal_phase0_reg = {i_lphy_sb_pkt_enc_srcid, 2'b00, i_lphy_sb_pkt_enc_tag, i_lphy_sb_pkt_enc_be,
+                             8'h00, i_lphy_sb_pkt_enc_ep, i_lphy_sb_pkt_enc_opcode};
+      // Phase 1: dp[31], cp[30], cr[29], rsvd[28:27], dstid[26:24], rsvd[23:3], cp_status[2:0]
+      internal_phase1_reg = {2'b00, i_lphy_sb_pkt_enc_cr, 2'b00, i_lphy_sb_pkt_enc_dstid,
+                             21'b0, i_lphy_sb_pkt_enc_cp_status};
+    end else begin
+      // Register Access Requests
+      // Phase 0: srcid[31:28], rsvd[28:27], tag[26:22], be[21:14], rsvd[13:6], ep[5], opcode[4:0]
+      internal_phase0_reg = {i_lphy_sb_pkt_enc_srcid, 2'b00, i_lphy_sb_pkt_enc_tag, i_lphy_sb_pkt_enc_be,
+                             8'h00, i_lphy_sb_pkt_enc_ep, i_lphy_sb_pkt_enc_opcode};
+      // Phase 1: dp[31], cp[30], cr[29], rsvd[28:27], dstid[26:24], addr[23:0]
+      internal_phase1_reg = {2'b00, i_lphy_sb_pkt_enc_cr, 2'b00, i_lphy_sb_pkt_enc_dstid,
+                             i_lphy_sb_pkt_enc_addr};
+    end  
+    internal_raw_header = {internal_phase1_reg, internal_phase0_reg};
+  end
+
+
 
 endmodule
