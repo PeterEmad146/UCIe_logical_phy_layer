761e33ea463fd73f1557bdc24d6f777d27069044
Peter Emad <peteremads1406@gmail.com>
2026-07-08 15:56:41 +0300

    Internal Signals Declaration and Decoding Opcodes to determine packet types

--- Logical_PHY/lphy_sb_pkt_enc.sv
+++ Logical_PHY/lphy_sb_pkt_enc.sv
@@ -33,7 +33,42 @@ module lphy_sb_pkt_enc(
   output logic [63:0] o_lphy_sb_pkt_enc_pkt_header,
   output logic [63:0] o_lphy_sb_pkt_enc_pkt_data, 
   output logic o_lphy_sb_pkt_enc_pkt_has_data
-
 );
+
+  logic is_reg_req;
+  logic is_reg_cpl;
+  logic is_msg;
+  logic has_data;
+  logic [31:0] phase0_reg;
+  logic [31:0] phase1_reg;
+  logic [63:0] raw_header;
+  logic [63:0] calc_header;
+
+  // Decode Opcode to determine packet type and data presense (Table 47)
+  always_comb begin
+    is_msg = (i_lphy_sb_pkt_enc_opcode == 5'b10010) || 
+             (i_lphy_sb_pkt_enc_opcode == 5'b11011);
+
+    has_data = (i_lphy_sb_pkt_enc_opcode == 5'b00001) ||   // 32b Mem Write
+               (i_lphy_sb_pkt_enc_opcode == 5'b00101) ||   // 32b Cfg Write
+               (i_lphy_sb_pkt_enc_opcode == 5'b01001) ||   // 64b Mem Write
+               (i_lphy_sb_pkt_enc_opcode == 5'b01101) ||   // 64b Cfg Write
+               (i_lphy_sb_pkt_enc_opcode == 5'b10001) ||   // Cpl with 32b Data
+               (i_lphy_sb_pkt_enc_opcode == 5'b11001) ||   // Cpl with 64b Data
+               (i_lphy_sb_pkt_enc_opcode == 5'b11011) ||   // Message with 64b Data
+
+    is_reg_req = (i_lphy_sb_pkt_enc_opcode == 5'b00000) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b00001) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b00100) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b00101) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b01000) || 
+                 (i_lphy_sb_pkt_enc_opcode == 5'b01001) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b01100) ||
+                 (i_lphy_sb_pkt_enc_opcode == 5'b01101);
+    
+    is_reg_cpl = (i_lphy_sb_pkt_enc_opcode == 5'b10000);
+                 (i_lphy_sb_pkt_enc_opcode == 5'b10001);
+                 (i_lphy_sb_pkt_enc_opcode == 5'b11001);
+  end
 
 endmodule
