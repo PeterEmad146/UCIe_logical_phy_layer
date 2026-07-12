e881416289fe2b821e7d60eab467cf0f54c27796
Peter Emad <peteremads1406@gmail.com>
2026-07-12 11:20:19 +0300

    Sideband Flow Control Module - Autorization Logic and Conter Updates Implementation

--- Logical_PHY/lphy_sb_flow_ctrl.sv
+++ Logical_PHY/lphy_sb_flow_ctrl.sv
@@ -32,6 +32,60 @@ module lphy_sb_flow_ctrl #(
   logic internal_consume_local;
   logic internal_consume_remote;
 
-  
-  
+  always_comb begin
+    internal_consume_local = 1'b0;
+    internal_consume_remote = 1'b0;
+
+    if(i_lphy_sb_flow_ctrl_req_valid && o_lphy_sb_flow_ctrl_tx_allowed) begin
+      // The Transmitter must not check for credits before sending Register Access Completions
+      if (i_lphy_sb_flow_ctrl_is_reg_req) begin
+        internal_consume_local = 1'b1;
+        internal_consume_remote = 1'b1;
+      end else if (i_lphy_sb_flow_ctrl_is_msg) begin
+        internal_consume_local = 1'b1;
+      end
+    end
+  end
+
+  // Authorizatoin logic
+  always_comb begin
+    if (i_lphy_sb_flow_ctrl_is_reg_cpl) begin
+      // Completions must always sink and do not require credits
+      o_lphy_sb_flow_ctrl_tx_allowed = 1'b0;
+    end else if (i_lphy_sb_flow_ctrl_is_reg_req) begin
+      // Needs both local RDI space and Remote E2E space
+      o_lphy_sb_flow_ctrl_tx_allowed = (internal_local_crd_count > 0) && (internal_remote_crd_count > 0);
+    end else if (i_lphy_sb_flow_ctrl_is_msg) begin
+      // Only needs local RDI space
+      o_lphy_sb_flow_ctrl_tx_allowed = (internal_local_crd_count > 0);
+    end else begin
+      o_lphy_sb_flow_ctrl_tx_allowed = 1'b0;
+    end
+  end
+
+  // Sequential Counter Updates
+  always_ff @(posedge i_lphy_sb_flow_ctrl_clk or negedge i_lphy_sb_flow_ctrl_rst_n) begin
+    if(!i_lphy_sb_flow_ctrl_rst_n) begin
+      internal_local_crd_count <= LOCAL_CREDITS_INIT[5:0];
+      internal_remote_crd_count <= 3'd4;
+    end else if (i_lphy_sb_flow_ctrl_rdi_in_reset) begin
+      // The Adapter credit counters for register access request transmission
+      // are initialized to 4 whenever RDI is in Reset state. 
+      internal_local_crd_count <= LOCAL_CREDITS_INIT[5:0];
+      internal_remote_crd_count <= 3'd4;
+    end else begin
+      // Local Credit Update
+      if (internal_consume_local && !i_lphy_sb_flow_ctrl_local_crd_ret)
+        internal_local_crd_count <= internal_local_crd_count - 1'b1;
+      else if (!internal_consume_local && i_lphy_sb_flow_ctrl_local_crd_ret 
+               && (internal_local_crd_count < LOCAL_CREDITS_INIT[5:0]))
+        internal_local_crd_count <= internal_local_crd_count + 1'b1;
+      // Remote Credit Update
+      if (internal_consume_remote && !i_lphy_sb_flow_ctrl_remote_crd_ret)
+        internal_remote_crd_count <= internal_remote_crd_count - 1'b1;
+      else if (!internal_consume_remote && i_lphy_sb_flow_ctrl_remote_crd_ret 
+               && (internal_remote_crd_count < 3'd4))
+        internal_remote_crd_count <= internal_remote_crd_count + 1'b1;
+    end
+  end
 endmodule
