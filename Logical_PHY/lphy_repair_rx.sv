7b9b92316cc914351e510d94bd67a771c09d8926
Peter Emad <peteremads1406@gmail.com>
2026-07-15 13:34:08 +0300

    Repair RX Module - Fixed Synatx Errors

--- Logical_PHY/lphy_repair_rx.sv
+++ Logical_PHY/lphy_repair_rx.sv
@@ -55,49 +55,49 @@ module lphy_repair_rx(
         end
       end
     end
+  end
 
-    // Group 2: Upper 32 Lanes (32 to 63) using RRD_P[7] and RRD_P[8]
-    logic [1:0] internal_fail_cnt_upper;
-    logic [5:0] internal_f0_u, internal_f1_u;
+  // Group 2: Upper 32 Lanes (32 to 63) using RRD_P[7] and RRD_P[8]
+  logic [1:0] internal_fail_cnt_upper;
+  logic [5:0] internal_f0_u, internal_f1_u;
+  always_comb begin
+    internal_fail_cnt_upper = 0;
+    internal_f0_u = '0;
+    internal_f1_u = '0;
 
-    always_comb begin
-      internal_fail_cnt_upper = 0;
-      internal_f0_u = '0;
-      internal_f1_u = '0;
-
-      // Count failiures and record their physical indices
-      for (int i = 32; i < 64; i++) begin
-        if (i_lphy_repair_rx_lane_failed[i]) begin
-          if (internal_fail_cnt_upper == 0) internal_f0_u = i[5:0];
-          else if (internal_fail_cnt_upper == 1) internal_f1_u = i[5:0];
-          internal_fail_cnt_upper++;
-        end
+    // Count failiures and record their physical indices
+    for (int i = 32; i < 64; i++) begin
+      if (i_lphy_repair_rx_lane_failed[i]) begin
+        if (internal_fail_cnt_upper == 0) internal_f0_u = i[5:0];
+        else if (internal_fail_cnt_upper == 1) internal_f1_u = i[5:0];
+        internal_fail_cnt_upper++;
       end
+    end
 
-      // Default 1:1 mapping
-      for (int i = 32; i < 64; i++) o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i];
+    // Default 1:1 mapping
+    for (int i = 32; i < 64; i++) o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i];
 
-      if (internal_fail_cnt_upper == 1) begin
-        // Single Failure: Reconstruct shift-right mapping
-        o_lphy_repair_rx_logical_data[32] = i_lphy_repair_rx_redundant_data[2];
-        for (int i = 33; i <= 63; i++) begin
-          if (i <= internal_f0_u)
-            o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i-1];
-        end
+    if (internal_fail_cnt_upper == 1) begin
+      // Single Failure: Reconstruct shift-right mapping
+      o_lphy_repair_rx_logical_data[32] = i_lphy_repair_rx_redundant_data[2];
+      for (int i = 33; i <= 63; i++) begin
+        if (i <= internal_f0_u)
+          o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i-1];
       end
+    end
 
-      else if (internal_fail_cnt_upper == 2) begin
-        // Two Failures: Reconstruct split shift mapping
-        o_lphy_repair_rx_logical_data[32] = i_lphy_repair_rx_redundant_data[2];
-        o_lphy_repair_rx_logical_data[63] = i_lphy_repair_rx_redundant_data[3];
+    else if (internal_fail_cnt_upper == 2) begin
+      // Two Failures: Reconstruct split shift mapping
+      o_lphy_repair_rx_logical_data[32] = i_lphy_repair_rx_redundant_data[2];
+      o_lphy_repair_rx_logical_data[63] = i_lphy_repair_rx_redundant_data[3];
 
-        for (int i = 33; i <= 62; i++) begin
-          if (i <= internal_f0_u)
-            o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i-1];
-          else if (i >= internal_f1_u)
-            o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i+1];
-        end
+      for (int i = 33; i <= 62; i++) begin
+        if (i <= internal_f0_u)
+          o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i-1];
+        else if (i >= internal_f1_u)
+          o_lphy_repair_rx_logical_data[i] = i_lphy_repair_rx_physical_data[i+1];
       end
     end
   end
+  
 endmodule
